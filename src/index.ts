0c03163f567c51e2b63e7d71b518e1babccdaccd
root <root@QuynhAnh.localdomain>
2025-06-12 09:07:53 +0000

    use middleware compression

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import express from 'express'
 import envConfig from '~/config/env.config'
 import helmet from 'helmet'
+import morgan from 'morgan'
+import compression from 'compression'
 
 const app = express()
 
 // init middleware
 app.use(helmet())
+app.use(morgan('dev'))
+// app.use(compression())
 
 // init db
 
 // init route
 app.get('/', (req, res) => {
+  const str = 'duongvinh'
   res.status(200).json({
-    message: 'Hello world'
+    message: 'Hello world',
+    metaData: str.repeat(100000)
   })
 })
 
