fd606ed7152968a49b323f0a6de1a0c95a1b6cf1
root <root@QuynhAnh.localdomain>
2025-06-12 08:46:59 +0000

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,21 @@
-const name = 'Duong Vinh'
-const a = 10
+import express from 'express'
+import envConfig from '~/config/env.config'
+import helmet from 'helmet'
+
+const app = express()
+
+// init middleware
+app.use(helmet())
+
+// init db
+
+// init route
+app.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello world'
+  })
+})
+
+app.listen(envConfig.PORT, () => {
+  console.log(`Server is running at http://localhost:${envConfig.PORT}`);
+})
